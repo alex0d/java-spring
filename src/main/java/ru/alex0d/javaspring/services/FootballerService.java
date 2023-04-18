91b0ae13b7b5e6981b6c6c3b0c37c551189aeaea
alex0d <alexey.d707@gmail.com>
2023-04-18 16:24:19 +0300

    Practice 19

--- src/main/java/ru/alex0d/javaspring/services/FootballerService.java
+++ src/main/java/ru/alex0d/javaspring/services/FootballerService.java
@@ -1,31 +1,40 @@
 package ru.alex0d.javaspring.services;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import ru.alex0d.javaspring.models.Footballer;
 import ru.alex0d.javaspring.models.Team;
 import ru.alex0d.javaspring.repositories.FootballerRepository;
 
 import java.util.List;
 
-@Component
+@Service
 @RequiredArgsConstructor
+@Slf4j
 public class FootballerService {
     private final FootballerRepository footballerRepository;
 
     public void createFootballer(Footballer footballer) {
+        log.info("Footballer created");
         footballerRepository.save(footballer);
     }
 
     public void deleteFootballerById(Long id) {
+        log.info("Footballer deleted by id: " + id);
         footballerRepository.deleteById(id);
     }
 
     public List<Footballer> filterFootballers(Long id, String firstName, String lastName, Team team) {
         if (id == null && firstName == null && lastName == null && team == null) {
+            log.info("Footballers filtered by all null: returning all");
             return footballerRepository.findAll();
         } else {
-            return footballerRepository.findByIdOrFirstNameContainingOrLastNameContainingOrTeam(id, firstName, lastName, team);
+            List<Footballer> footballers = footballerRepository.findByIdOrFirstNameContainingOrLastNameContainingOrTeam(id, firstName, lastName, team);
+            List<Long> ids = footballers.stream().map(Footballer::getId).toList();
+            Long teamId = team == null ? null : team.getId();
+            log.info("Footballers filtered by id=" + id + " firstName=" + firstName + " lastName=" + lastName + " teamId=" + teamId + ": " + ids);
+            return footballers;
         }
     }
 }
--- src/main/java/ru/alex0d/javaspring/services/TeamService.java
+++ src/main/java/ru/alex0d/javaspring/services/TeamService.java
@@ -1,31 +1,39 @@
 package ru.alex0d.javaspring.services;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import ru.alex0d.javaspring.models.Team;
 import ru.alex0d.javaspring.repositories.TeamRepository;
 
 import java.time.LocalDate;
 import java.util.List;
 
-@Component
+@Service
 @RequiredArgsConstructor
+@Slf4j
 public class TeamService {
     private final TeamRepository teamRepository;
 
     public void createTeam(Team team) {
+        log.info("Team created");
         teamRepository.save(team);
     }
 
     public void deleteTeamById(Long id) {
+        log.info("Team deleted by id: " + id);
         teamRepository.deleteById(id);
     }
 
     public List<Team> filterTeams(Long id, String name, LocalDate creationDate) {
         if (id == null && name == null && creationDate == null) {
+            log.info("Teams filtered by all null: returning all");
             return teamRepository.findAll();
         } else {
-            return teamRepository.findByIdOrNameContainingOrCreationDate(id, name, creationDate);
+            List<Team> teams = teamRepository.findByIdOrNameContainingOrCreationDate(id, name, creationDate);
+            List<Long> ids = teams.stream().map(Team::getId).toList();
+            log.info("Teams filtered by id=" + id + " name=" + name + " creationDate=" + creationDate + ": " + ids);
+            return teams;
         }
     }
 }
